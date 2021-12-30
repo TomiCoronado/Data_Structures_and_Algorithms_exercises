f7cda31ca79c3153e2c854779dbc259fee65b48f
Tomas Coronado Gonzalez <60554231+TomiCoronado@users.noreply.github.com>
2021-12-31 00:05:39 +0100

    Update 3b_check_if_a_given_string_is_palindrome.cpp

--- 3b_check_if_a_given_string_is_palindrome.cpp
+++ 3b_check_if_a_given_string_is_palindrome.cpp
@@ -6,15 +6,15 @@
  * 2) Else compare first and last characters and recur for 
  *    remaining substring.*/
 
-// La resolución con el método iterativo la tengo en C y esta en C++
-#include<bits/stdc++.h> // En C necesito stdio.h y string.h 
-                        // Este último xq llamo a strlen()
-// En C no existe el tipo bool pero puedo usar las funciones de abajo si hago esto:
+// Now we are writting the program in C++
+#include<bits/stdc++.h> //C: stdio.h and string.h 
+
+// En C we don't have the bool type but can use the funtions below if we include:
 // #include<stdbool.h>
 using namespace std;
 
 // A recursive function that
-// check a str[s..e] is
+// checks a str[s..e] is
 // palindrome or not.
 bool isPalRec(char str[], int s, int e){
     // If there is only one character
@@ -26,15 +26,16 @@ bool isPalRec(char str[], int s, int e){
     // If there is more than two characters, check if middle substring
     // is also palindrome or not
     if (s<e+1) return isPalRec(str, s+1, e-1);
+  
+    // We recursively call the function till there is only character 
+    // (in that case we return true), or we find a couple of elements 
+    // that are not equal (in tath case we return false), or we finish
+    // successfully comparing each couple
 
     return true;
 }
-// creo que, como hemos puesto un return en cada if, pues no me hace falta 
-// ir poniendo else if dado que si un if se cumple salimos de la funcion y 
-// el resto no se evaluan
-
-// La anterior función recursiva creo que acaba en el momento en el que devolvemos
-// algún return true; o retrun false;
+// In every recursive funtion its important to identify the recursive process 
+// as well as the termination conditions
 
 bool isPalindrome(char str[]){
     int n = strlen(str);
