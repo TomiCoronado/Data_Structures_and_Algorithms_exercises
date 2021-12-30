bf4b13912e1ced1d0edbb03265a282e8e40714c0
Tomas Coronado Gonzalez <60554231+TomiCoronado@users.noreply.github.com>
2021-12-30 23:31:03 +0100

    Update 2b_sum_of_digits.cpp

--- 2b_sum_of_digits.cpp
+++ 2b_sum_of_digits.cpp
@@ -1,25 +1,26 @@
 /* Sum of digits 
 Given a number, find sum of its digits.
-También he decidido que el fichero tenga extensión .cpp
 
 When the number of digits of that number exceeds 10^19 , we can’t take 
 that number as integer since the range of long long int doesn’t satisfy 
 the given number. So take input as a string, run a loop from start to the 
 length of the string and increase the sum with that character 
-(in this case it is numeric)
+(in this case it is numeric).
+This is an interesting way to store a big number.
 */
 
 #include <iostream>     // C: #include<stdio.h>
-using namespace std;    // C: sobra esta linea
-int getSum(string str)  // C: en C no existe el tipo de dato string sino  
-{                       //    el char. By convention, C uses arrays of char 
-	int sum = 0;        //    terminated with a null char, i.e., with '\0'.
+using namespace std;    // C: this line is not necessary
+int getSum(string str)  // C: By convention, C uses arrays of char 
+			//    in C we don't have the string type
+	int sum = 0;    // Terminated with a null char, i.e., with '\0'.
 
 	// Traversing through the string
-	for (int i = 0; i < str.length(); i++) {    // Creo que .length es un method 
-		// Since ascii value of                 // que se puede aplicar a strings
+	for (int i = 0; i < str.length(); i++) {    // .length is a usefull method for stings in C++
+		// Since ascii value of             
 		// numbers starts from 48
 		// so we subtract it from sum
+		// (str[i] is the ascii the numeration of character i in the ascii table)
 		sum = sum + str[i] - 48;
 	}
 	return sum;
