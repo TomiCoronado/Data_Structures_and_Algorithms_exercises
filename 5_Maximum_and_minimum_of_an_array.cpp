6160d623c17099949b45c99596b71988b66942b2
Tomas Coronado Gonzalez <60554231+TomiCoronado@users.noreply.github.com>
2021-12-31 12:48:11 +0100

    Update 5_Maximum_and_minimum_of_an_array.cpp

--- 5_Maximum_and_minimum_of_an_array.cpp
+++ 5_Maximum_and_minimum_of_an_array.cpp
@@ -49,6 +49,13 @@ Thus, the approach does 3n/2 - 2 comparisons if n is a power of 2. And it does m
 than 3n/2 - 2 comparisons if n is not a power of 2.
 */
 
+/* METHOD 3 (Compare in Pairs) 
+If n is odd then initialize min and max as first element. 
+If n is even then initialize min and max as minimum and maximum of the first two elements respectively. 
+For rest of the elements, pick them in pairs and compare their 
+maximum and minimum with max and min respectively.
+*/
+
 // C++ program of above implementation
 #include<iostream>  // C: #include<stdio.h>
 using namespace std;
@@ -94,7 +101,7 @@ struct Pair getMinMax_v1(int arr[], int n){
 }
 
 // METHOD 2
-struct Pair getMinMax(int arr[], int low, int high)
+struct Pair getMinMax_v2(int arr[], int low, int high)
 {
     struct Pair minmax, mml, mmr;    
     int mid;
@@ -125,8 +132,8 @@ struct Pair getMinMax(int arr[], int low, int high)
      
     // If there are more than 2 elements
     mid = (low + high) / 2;
-    mml = getMinMax(arr, low, mid);
-    mmr = getMinMax(arr, mid + 1, high);
+    mml = getMinMax_v2(arr, low, mid);
+    mmr = getMinMax_v2(arr, mid + 1, high);
      
     // Compare minimums of two parts
     if (mml.min < mmr.min)
@@ -140,6 +147,73 @@ struct Pair getMinMax(int arr[], int low, int high)
     else
         minmax.max = mmr.max;    
      
+    return minmax;
+}
+
+// METHOD 3
+struct Pair getMinMax_v3(int arr[], int n)
+{
+    struct Pair minmax;    
+    int i;
+     
+    // If array has even number of elements
+    // then initialize the first two elements
+    // as minimum and maximum
+    if (n % 2 == 0)
+    {
+        if (arr[0] > arr[1])    
+        {
+            minmax.max = arr[0];
+            minmax.min = arr[1];
+        }
+        else
+        {
+            minmax.min = arr[0];
+            minmax.max = arr[1];
+        }
+         
+        // Set the starting index for loop
+        i = 2;
+    }
+     
+    // If array has odd number of elements
+    // then initialize the first element as
+    // minimum and maximum
+    else
+    {
+        minmax.min = arr[0];
+        minmax.max = arr[0];
+         
+        // Set the starting index for loop
+        i = 1;
+    }
+     
+    // In the while loop, pick elements in
+    // pair and compare the pair with max
+    // and min so far
+    while (i < n - 1)
+    {        
+        if (arr[i] > arr[i + 1])        
+        {
+            if(arr[i] > minmax.max)    
+                minmax.max = arr[i];
+                 
+            if(arr[i + 1] < minmax.min)        
+                minmax.min = arr[i + 1];    
+        }
+        else       
+        {
+            if (arr[i + 1] > minmax.max)    
+                minmax.max = arr[i + 1];
+                 
+            if (arr[i] < minmax.min)        
+                minmax.min = arr[i];    
+        }
+         
+        // Increment the index by 2 as
+        // two elements are processed in loop
+        i += 2;
+    }        
     return minmax;
 }
 
